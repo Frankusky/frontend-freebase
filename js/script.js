67cc1415d4ba0dfd4ac9f1dcc07d875ca3e6191a
Frank Sam Hu <fsh1991@hotmail.com>
2014-12-03 11:40:06 -0600

    First commit

--- js/script.js
+++ js/script.js
@@ -1,24 +1,81 @@
-$( document ).ready(function() {
+var busqueda = "";
+$( ".formulario" ).submit(function( event ) {
+	$(".informacion").empty();
+	busqueda = $(".texto").val();
+	$(".texto").val("");
+	var service_url = 'https://www.googleapis.com/freebase/v1/search';
+	var params = {
+		'query': busqueda,
+		//'filter': '(all type:/music/artist)',
+		'limit': 10,
+		'indent': true
+	};
 
-  var service_url = 'https://www.googleapis.com/freebase/v1/search';
+	$.getJSON(service_url + '?callback=?', params, function(response) {
+		var numero = 0;
+		$.each(response.result, function(i, result) {
+			numero+=1;
+			var obj = {text:result['name']}.text;
+			var insertar = "<tr>"+"<td>"+numero+"<td>"+obj+"</td>"+"</tr>";
+			$(".informacion").append(insertar);
+		});
+	});
+	$('.check').prop('checked', false);
+	event.preventDefault();
 
-  $("#search-button").click( function() {
-    var query = $( "input[name='search']" ).val();
-    alert(query);
+	
+});
 
-    var params = {
-      'query': query,
-      'filter': '(all type:/music/artist )',
-      'limit': 10,
-      'indent': true
-    };
+$('input[name="filtros"]').change(function(){
+	$(".informacion").empty();
+	var checkedEnable = []; //0,1,2
+	var filterUrls = [" type:/music/artist", " type:/people/person", " type:/book/"];
+	var filterString = "";
+	$("input[name=filtros]:checked").each(function(){
+		checkedEnable.push(this.value);
+	});
+	if(checkedEnable.length==0){
+		$(".disable").slideUp("slow");
+	}else{
+		$(".panel-heading").css("display","block");
+		$(".panel-body").css("display","block");
+		if(checkedEnable.indexOf("0")>=0){
+			$(".subfilterMusic").slideDown("slow");
+		}else{
+			$(".subfilterMusic").slideUp("slow");
+		};
+		if(checkedEnable.indexOf("1")>=0){
+			$(".subfilterPeople").slideDown("slow");
+		}else{
+			$(".subfilterPeople").slideUp("slow");
+		};
+		if(checkedEnable.indexOf("2")>=0){
+			$(".subfilterBooks").slideDown("slow");
+		}else{
+			$(".subfilterBooks").slideUp("slow");
+		};
+	};
 
-    $.getJSON(service_url + '?callback=?', params, function(response) {
-      alert(response);
-      $.each(response.result, function(i, result) {
-        $('<div>',{text:result['name']}, '</div>').appendTo(".informacion");
-      });
-    });
-  });
-
+	for (var i=0;i<checkedEnable.length;i++){
+		filterString = filterString + filterUrls[checkedEnable[i]];
+	};
+	
+	filterString = "(all"+filterString+")";
+	var service_url = 'https://www.googleapis.com/freebase/v1/search';
+	var params = {
+		'query': busqueda,
+		'filter': filterString,
+		'limit': 10,
+		'indent': true
+	};
+	$.getJSON(service_url + '?callback=?', params, function(response) {
+		var numero = 0;
+		$.each(response.result, function(i, result) {
+			numero+=1;
+			var obj = {text:result['name']}.text;
+			var insertar = "<tr>"+"<td>"+numero+"<td>"+obj+"</td>"+"</tr>";
+			$(".informacion").append(insertar);
+		});
+	});
+	event.preventDefault();
 });
